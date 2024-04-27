a91de9f7fa852479f50ceff63044437745e75de3
Harrbinger <massimochiarell@gmail.com>
2024-04-27 17:13:22 -0400

    Write initial UI and the ability to input choices

---
+++ supermarket-system.py
@@ -0,0 +1,4 @@
+items=[]
+print("----------- Welcome to the supermarket -----------")
+print("1. View items /n 2. Add items /n 3. Purchase items /n 4. Search items /n 5.Edit items /n 6. Exit")
+Choice=int(input("Enter the number of your choice: "))
