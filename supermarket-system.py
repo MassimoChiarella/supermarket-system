06ab2469a9b39bacf31a616092fe35d831173c70
Harrbinger <massimochiarell@gmail.com>
2024-04-27 19:46:33 -0400

    Develop option to add new items to the inventory

--- supermarket-system.py
+++ supermarket-system.py
@@ -1,7 +1,7 @@
 items=[]
 print("----------- Welcome to the supermarket -----------")
 print("1. View items /n 2. Add items /n 3. Purchase items /n 4. Search items /n 5.Edit items /n 6. Exit")
-Choice=int(input("Enter the number of your choice: "))
+choice=int(input("Enter the number of your choice: "))
 if Choice == 1:
     print("----------- View Items -----------")
     print("The number of items in the inventory are %d" %len(items))
@@ -10,3 +10,22 @@ if len(items)!=0:
     for item in items:
         for key, value in item.items():
             print ("%s : %S" %(key, value))
+elif choice ==2:
+    print("----------- Add Items -----------")
+    print("To add an item, fill in the form")
+    item={}
+    item["name"] = input("item name: ")
+    while True:
+        try:
+            item["quantity"]= int(input("Item quantity"))
+            break
+        except ValueError:
+            print("Quantity should only be a number")
+    while True:
+        try:
+            item["price"]= int(input("Item price"))
+            break
+        except ValueError:
+            print("Price should only be a number")
+    print("Item has been successfully added!")
+    items.append(item)
